c2db129c235a5d70de6454c39a14ba2fe61ac74a
angelinabriciene <aburbaite865@gmail.com>
2024-08-22 19:41:19 +0300

    + Many to one/ invoice to type

--- src/main/java/com/example/rupija/models/Type.java
+++ src/main/java/com/example/rupija/models/Type.java
@@ -17,6 +17,7 @@ import java.util.List;
 @Setter
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Type {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
